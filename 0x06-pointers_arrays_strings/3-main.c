c3c05b3d0c8501ef69b0f543f7f6e4e6f6b7b841
AbdelghanyElattari12 <prisebyed@gmail.com>
2023-03-30 05:11:15 -0700

    naii

---
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[] = "aHello";
+	char s2[] = "aWorld!";
+
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Statu
